98cab94c498a80835296aa18fa568f60d632f8f9
Daniel Molnar <dm@tpwd.de>
2020-12-03 19:27:03 +0100

    chore: provide MatomoContext
    - export `MatomoContext` as well

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import MatomoTracker from './src/MatomoTracker';
-import MatomoProvider from './src/MatomoProvider';
+import MatomoProvider, { MatomoContext } from './src/MatomoProvider';
 import useMatomo from './src/useMatomo';
 
 export default MatomoTracker;
-export { MatomoProvider, useMatomo };
+export { MatomoProvider, MatomoContext, useMatomo };
