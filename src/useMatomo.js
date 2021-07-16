603d538e45a133251fca1af4bf012353ec1889b2
Daniel Molnar <dm@tpwd.de>
2021-07-16 21:32:39 +0200

    refactor(useMatomo hook): use useMemo instead of useCallback
    - added `useMemo` in favor of `useCallback` for memoizing
      the whole object and better readability of the code
    
    #4

--- src/useMatomo.js
+++ src/useMatomo.js
@@ -1,49 +1,22 @@
-import { useCallback, useContext } from 'react';
+import { useContext, useMemo } from 'react';
 
 import { MatomoContext } from './MatomoProvider';
 
 const useMatomo = () => {
   const instance = useContext(MatomoContext);
 
-  const trackAppStart = useCallback(() => instance.trackAppStart && instance.trackAppStart(), [
-    instance
-  ]);
-
-  const trackScreenView = useCallback(
-    (params) => instance.trackScreenView && instance.trackScreenView(params),
+  return useMemo(
+    () => ({
+      trackAppStart: () => instance.trackAppStart && instance.trackAppStart(),
+      trackScreenView: (params) => instance.trackScreenView && instance.trackScreenView(params),
+      trackAction: (params) => instance.trackAction && instance.trackAction(params),
+      trackEvent: (params) => instance.trackEvent && instance.trackEvent(params),
+      trackSiteSearch: (params) => instance.trackSiteSearch && instance.trackSiteSearch(params),
+      trackLink: (params) => instance.trackLink && instance.trackLink(params),
+      trackDownload: (params) => instance.trackDownload && instance.trackDownload(params)
+    }),
     [instance]
   );
-
-  const trackAction = useCallback(
-    (params) => instance.trackAction && instance.trackAction(params),
-    [instance]
-  );
-
-  const trackEvent = useCallback((params) => instance.trackEvent && instance.trackEvent(params), [
-    instance
-  ]);
-
-  const trackSiteSearch = useCallback(
-    (params) => instance.trackSiteSearch && instance.trackSiteSearch(params),
-    [instance]
-  );
-
-  const trackLink = useCallback((params) => instance.trackLink && instance.trackLink(params), []);
-
-  const trackDownload = useCallback(
-    (params) => instance.trackDownload && instance.trackDownload(params),
-    [instance]
-  );
-
-  return {
-    trackAppStart,
-    trackScreenView,
-    trackAction,
-    trackEvent,
-    trackSiteSearch,
-    trackLink,
-    trackDownload
-  };
 };
 
 export default useMatomo;
